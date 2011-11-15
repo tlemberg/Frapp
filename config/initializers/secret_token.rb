3aea619ce470e2276f46411fd1898330306fdded
Tom Lemberg <tlemberg10@gmail.com>
2011-11-15 14:51:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frapp::Application.config.secret_token = '94d7879401e7da0deda2737238d556ecc686a6940489488083a6e89761b55fda7003447dd7d23ee91a73332f9eeea785d170ba6ae71331e575257bf380bcdaf3'
